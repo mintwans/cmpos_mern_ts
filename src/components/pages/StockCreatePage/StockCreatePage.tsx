45bf0b4d27d3b3cd2e7e896376210e32e3dbbcf5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-20 10:51:57 +0700

    1234

--- src/components/pages/StockCreatePage/StockCreatePage.tsx
+++ src/components/pages/StockCreatePage/StockCreatePage.tsx
@@ -24,53 +24,24 @@ export default () => {
               Create Stock
             </Typography>
 
-            <Field
-              style={{ marginTop: 16 }}
-              fullWidth
-              component={TextField}
-              name="name"
-              type="text"
-              label="Name"
-            />
+            <Field style={{ marginTop: 16 }} fullWidth component={TextField} name="name" type="text" label="Name" />
             <br />
-            <Field
-              style={{ marginTop: 16 }}
-              fullWidth
-              component={TextField}
-              name="price"
-              type="number"
-              label="Price"
-            />
+            <Field style={{ marginTop: 16 }} fullWidth component={TextField} name="price" type="number" label="Price" />
 
-            <Field
-              style={{ marginTop: 16 }}
-              fullWidth
-              component={TextField}
-              name="stock"
-              type="number"
-              label="Stock"
-            />
+            <Field style={{ marginTop: 16 }} fullWidth component={TextField} name="stock" type="number" label="Stock" />
 
             <div style={{ margin: 16 }}>{showPreviewImage(values)}</div>
 
             <div>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/ic_photo.png`}
-                style={{ width: 25, height: 20 }}
-              />
-              <span style={{ color: "#00B0CD", marginLeft: 10 }}>
-                Add Picture
-              </span>
+              <img src={`${process.env.PUBLIC_URL}/images/ic_photo.png`} style={{ width: 25, height: 20 }} />
+              <span style={{ color: "#00B0CD", marginLeft: 10 }}>Add Picture</span>
 
               <input
                 type="file"
                 onChange={(e: React.ChangeEvent<any>) => {
                   e.preventDefault();
                   setFieldValue("file", e.target.files[0]); // for upload
-                  setFieldValue(
-                    "file_obj",
-                    URL.createObjectURL(e.target.files[0])
-                  ); // for preview image
+                  setFieldValue("file_obj", URL.createObjectURL(e.target.files[0])); // for preview image
                 }}
                 name="image"
                 click-type="type1"
@@ -82,13 +53,7 @@ export default () => {
             </div>
           </CardContent>
           <CardActions>
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-              sx={{ marginRight: 1 }}
-            >
+            <Button fullWidth variant="contained" color="primary" type="submit" sx={{ marginRight: 1 }}>
               Create
             </Button>
             <Button component={Link} to="/stock" variant="outlined" fullWidth>
@@ -114,10 +79,8 @@ export default () => {
         validate={(values) => {
           let errors: any = {};
           if (!values.name) errors.name = "Enter name";
-          if (values.stock < 10)
-            errors.stock = "Min stock is not lower than 10";
-          if (values.price < 100)
-            errors.price = "Min price is not lower than 100";
+          if (values.stock < 10) errors.stock = "Min stock is not lower than 10";
+          if (values.price < 100) errors.price = "Min price is not lower than 100";
           return errors;
         }}
         initialValues={initialValues}
